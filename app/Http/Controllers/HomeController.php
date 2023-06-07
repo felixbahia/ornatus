762a8d21169cf323df91f48ca2fc53c8038cc1b5
danilo.bahia <danilo.bahia@tecidosmn.com.br>
2023-06-07 17:25:54 -0300

    Criando cadastro de produtos e inserindo dados

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Preco;
+use App\Produto;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -24,5 +26,24 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+
+    public function inserirDados(){
+        $produtos = new Produto;
+        $produtos->codigo = 'VJ8756';
+        $produtos->descricao = 'Aliança de Ouro Classic';
+        $produtos->foto = 'foto/alianca.jpg';
+        $produtos->created_by = 1;
+        $produtos->save();
+
+        $preco = new Preco;
+        $preco->produto_id = $produtos->id;
+        $preco->dolar = 2045.92; 
+        $preco->real = 4045.92;
+        $preco->custo_real = 2000.92;
+        $preco->custo_dolar = 1000.92;
+        $preco->created_by = 1;
+        $preco->save();
+
     }
 }
---
+++ database/migrations/2023_06_07_164638_CriarTabelaProdutos.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CriarTabelaProdutos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('codigo');
+            $table->string('descricao');
+            $table->string('foto');
+            $table->integer('created_by')->unsigned();
+            $table->integer('updated_by')->unsigned()->nullable();
+            $table->integer('deleted_by')->unsigned()->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('created_by')
+                ->references('id')
+                ->on('users')
+                ->onDelete('NO ACTION');
+            $table->foreign('updated_by')
+                ->references('id')
+                ->on('users')
+                ->onDelete('NO ACTION');
+            $table->foreign('deleted_by')
+                ->references('id')
+                ->on('users')
+                ->onDelete('NO ACTION');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+}
